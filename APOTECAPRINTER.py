cd62f50a756fcba25276b13caef1299f3054339f
debuggingahopper <nelson.j.alvarez01@gmail.com>
2024-05-23 23:08:45 -0400

    Minor change

--- APOTECAPRINTER.py
+++ APOTECAPRINTER.py
@@ -1,4 +1,5 @@
 import subprocess
+import tkinter as tk
 from tkinter import *
 import pyuac
 
@@ -9,6 +10,8 @@ def main():
     # Adjust size 
     root.geometry( "200x200" ) 
     
+    # Set the original text
+    greeting = tk.Label(text="Welcome to the APOTECA Printer Assistant, Please select the printer model you would like to fix")
     # Change the label text 
     def show(): 
         sol = clicked.get()
@@ -17,7 +20,7 @@ def main():
         
     # Dropdown menu options 
     options = [ 
-        "Fix Printer", 
+        "PDFPrinter", 
         "Acquire PS Info"
     ] 
   
@@ -25,7 +28,7 @@ def main():
     clicked = StringVar() 
     
     # initial menu text 
-    clicked.set( "What would you like to do?" ) 
+    clicked.set( "What would you like to select?" ) 
     
     # Create Dropdown menu 
     drop = OptionMenu( root , clicked , *options ) 
@@ -37,33 +40,6 @@ def main():
     
     # Execute tkinter 
     root.mainloop() 
-    
-
-
-
-    '''
-    What we would like is to not only stop the spooler, and print a test page but also just change the tcp port address
-    I believe it is possible through the following line of code(https://superuser.com/questions/61659/how-do-i-add-a-standard-tcp-ip-printer-port-from-a-command-line):
-
-    rundll32 printui.dll,PrintUIEntry /if /b "PDFPrinter" /f %windir%\inf\ntprint.inf /r  "IP_157.57.50.98" /m "HP Laserjet 4000 Series PCL"
-
-    Now the main challange is the following finding the correct printer. You see what I would like is that is for the PS systems to allow the user to select the correct 
-    printer to do the tasks. This is something I would like to automate quickly, i wonder how many printers the PS systems display through CMD to see if we can just quickly query how many and then allow the user to select the correct one
-    and then from there select the correct printerIP.
-
-    Overall We would like the printername regardless so the rest of our scripts can work as expected
-
-
-    To place it as a parameter all I need is:
-    from subprocess import *
-p = Popen(['run-client.bat', param1, param2], stdout=PIPE, stderr=PIPE)
-output, errors = p.communicate()
-p.wait() # wait for process to terminate
-
-Where in the batch I just need to use %1 to reference ou string input
-
-What We really want is to just effeciently grab the printer names
-    '''
 
 if __name__ == "__main__":
     if not pyuac.isUserAdmin():
