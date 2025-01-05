fbf435cb5a939081398dcfc8b6cb0c4af4517d56
DebuggingaHopper <49813790+DebuggingaHopper@users.noreply.github.com>
2025-01-05 12:24:10 -0500

    Update Test.py
    removed unnecessary comments

--- Test.py
+++ Test.py
@@ -1,9 +1,5 @@
 import subprocess
 import pyuac
-# What this script proves is that yhe app crashes because it relies on the print spooler service running at all times
-# When it's restarted then the app freezes because its trying to grab the necessray info to display 
-
-# What was dteermined that we needed to run a seperate thread for this asyncrhonous call 
 def main():
     subprocess.run(r"Status.bat"+" "+ "PDFPrinter",shell=True, check=True)
 
